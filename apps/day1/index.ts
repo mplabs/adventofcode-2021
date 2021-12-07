989140952390481db0ace905f91f6df3a735d2db
Felix Dürrwald <mplabs@mplabs.de>
2021-12-07 11:04:22 +0100

    Refactor

---
+++ apps/day1/index.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from 'fs'
+
+const input = readFileSync(`${__dirname}/input.txt`, 'utf-8')
+  .split(/\r?\n/)
+  .map((str) => Number(str))
+
+const countIncreased = (input: number[]) =>
+  input.reduce(
+    (prev, curr, idx, arr) =>
+      arr[idx - 1] && arr[idx - 1] < arr[idx] ? prev + 1 : prev,
+    0
+  )
+
+function slidingWindow(input: number[]): number[] {
+  const output: number[] = []
+
+  for (let i = 0; i < input.length - 2; i++) {
+    const window: number[] = input.slice(i, i + 3)
+    output.push(window.reduce((p, c) => p + c, 0))
+  }
+
+  return output
+}
+
+console.log(countIncreased(slidingWindow(input)))
